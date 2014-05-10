0b4e4786f5b0fe976064651f95551e5a456fe36b
Austin <austin.haedicke@gmail.com>
2014-05-10 00:28:56 -0400

    readme update and wifi scripts

---
+++ README.md
@@ -0,0 +1,10 @@
+Scripts
+====================
+
+by grandtheftjiujitsu
+   
+------------------
+
+### Current Projects:
+> Making Demonstration / Educational Scripts as I read:
+> ![Picture](http://ecx.images-amazon.com/images/I/5173Q4Lj1JL.jpg)
---
+++ wifi-wpa.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# For use with "iw"
+
+iw dev
+echo -e "Enter Interface Name:" && read interface
+
+echo "Checking Interface Status..."
+iw dev $interface link
+
+echo "Activating Interface..."
+ip link set $interface up
+
+echo "Checking that Interface is Up..."
+ip link show $interface
+
+echo "Scanning for Access Point..."
+iw dev $interface scan| grep SSID
+
+echo -e "Enter Network Name:" && read ssid
+
+echo "Setting ad hoc Network Mode..."
+iw dev $interface set type ibss
+
+echo -e "Enter Password for Network ($ssid):"  && read -s psk
+touch /etc/wpa_suppliant/$ssid
+echo "network={
+  ssid=\"$ssid\"
+  psk=\"$psk\"
+}" > /etc/wpa_supplicant/$ssid
+
+wpa_supplicant -B -i $interface -c /etc/wpa_supplicant/$ssid
+
+dhcpcd $interface
+
+ping -c 3 www.google.com
