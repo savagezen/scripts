4012db7ab7c9aff6ba64122059dd2cf7e62b0d54
Austin <austin.haedicke@gmail.com>
2014-05-21 14:00:37 -0400

    Kelvin temperature conversion

--- README.md
+++ README.md
@@ -4,13 +4,23 @@ Scripts
 by grandtheftjiujitsu
    
 ------------------
+### Utilities:
+* [Simple CD / DVD rip using "dd"](https://github.com/grandtheftjiujitsu/Scripts/blob/master/dvdrip.sh)
+* [System Information](https://github.com/grandtheftjiujitsu/Scripts/blob/master/sysinfo.sh)
+* [Unit Conversions](https://github.com/grandtheftjiujitsu/Scripts/blob/master/unit_coversion.py)
+>* Temperature:  Celcius, Fahrenheit, Kelvin
+>* Length: Mile, Kilometer, Meter, Yard, Feet, Inches, Centimeter.
+* [WPA / Wifi setup using "iw" and "dhcpcd"](https://github.com/grandtheftjiujitsu/Scripts/blob/master/wifi-wpa.sh)
+* [Climbing Grade Conversions](https://github.com/grandtheftjiujitsu/Scripts/blob/master/wifi-wpa.sh)
+>* Yosemite Decimal System (YDS)
+>* Hueco
 
-### Current Projects:
-* Making Demonstration / Educational Scripts as I read:
+### Current Homework:
+* Making Demonstration / Educational Scripts while reading:
 
 ![Picture](http://ecx.images-amazon.com/images/I/5173Q4Lj1JL.jpg)
 
-* Started an account on codecademy.com to learn basic Python
+* codecademy Python Course
 
 ![Picture](http://www.codecademy.com/assets/logo/logo--dark-blue.svg)
 ![Picture](http://www.bopen.eu/technologies/open-source-technologies/python-programming-language/content_logo)
--- unit_coversion.py
+++ unit_coversion.py
@@ -9,21 +9,46 @@ print "	[4] Volume"
 print "	[5] Digital Storage"
 conv_type = raw_input("Select conversion type: ")
 
-# Temnperature
+# Temperature
 if conv_type == "Temperature" or "1":
-	base = raw_input("Enter base unit [Celcius / Fahrenheit]: ")
+	base = raw_input("Enter base unit [Celcius / Fahrenheit / Kelvin]: ")
+	value = int(raw_input("Enter vase value: "))
+	target = raw_input("Enter target unit: ")
 	if base == "Celcius":
-		value = int(raw_input("Enter value: "))
-		value = value * 9
-		value = value / 5
-		value = value + 32
-		print " %d Degrees Fahrenheit" % value
-	elif:
-		value = int(raw_input("Enter value: "))
-		value = value - 32
-		value = value * 5
-		value = value / 9
-		print " %d Degrees Celcius" % value
+		if target == "Fahrenheit":
+			value = value * 9
+			value = value / 5
+			value = value + 32
+			print " %d Degrees Fahrenheit" % value
+		elif target == "Kelvin":
+			value = value + 273.15
+			print " %k Kelvin" % value
+		else:
+			print "Not a valid choice."
+	elif base == "Fahrenheit":
+		if target == "Celcius":
+			value = int(raw_input("Enter base value: "))
+			value = value - 32
+			value = value * 5
+			value = value / 9
+			print " %d Degrees Celcius" % value
+		elif target == "Kelvin":
+			value = value -32
+			value = value * 5
+			value = value / 9
+			value = valu + 273
+			print " %k Kelvin" % value
+	elif base == "Kelvin":
+		if target == "Celcius":
+			value = value - 273.15
+			print " %c Degrees Celcius" % value
+		elif target == "Fahrenheit":
+			value = value - 305.15
+			value = value * 5
+			value = value / 9
+			print " %f Degrees  Fahrenheit" % value
+		else:
+			print "Not a valid choice."
 	else:
 		print "Not a valid choice"
 
@@ -87,126 +112,126 @@ elif conv_type == "Length" or "2":
 	elif base == "Meter":
 		value = int(raw_input("Enter base value: "))
 		target == raw_input("Enter target unit: ")
-			if target == "Kilometer":
-				value = value * 0.001
-				print " %k Kilometers" % value
-			elif target == "Mile":
-				value = value * 0.000621371
-				print " %m Miles" % value
-			elif target == "Yard":
-				value = value * 1.09361
-				print " %y Yards" % value
-			elif target == "Foot":
-				value = value * 3.28084
-				print " %f Feet" % value
-			elif target == "Inch":
-				value = value * 39.3701
-				print " %i Inches" % value
-			elif target == "Centimeter":
-				value = value * 100
-				print " %c Centimeters" % value
-			else:
-				print "Not a valid choice."
+		if target == "Kilometer":
+			value = value * 0.001
+			print " %k Kilometers" % value
+		elif target == "Mile":
+			value = value * 0.000621371
+			print " %m Miles" % value
+		elif target == "Yard":
+			value = value * 1.09361
+			print " %y Yards" % value
+		elif target == "Foot":
+			value = value * 3.28084
+			print " %f Feet" % value
+		elif target == "Inch":
+			value = value * 39.3701
+			print " %i Inches" % value
+		elif target == "Centimeter":
+			value = value * 100
+			print " %c Centimeters" % value
+		else:
+			print "Not a valid choice."
 	elif base == "Yard":
 		value = int(raw_input("Enter base value: "))
 		target == raw_input("Enter target unit: ")
-			if target == "Kilometer":
-				value = value * 0.0009144
-				print " %k Kilometers" % value
-			elif target == "Mile":
-				value = value * 0.000568182
-				print " %m Miles" % value
-			elif target == "Meter":
-				value = value * 0.9144
-				print " %m Meters" % value
-			elif target == "Foot":
-				value = value * 3
-				print " %f Feet" % value
-			elif target == "Inch":
-				value = value * 36
-				print " %i Inches" % value
-			elif target == "Centimeter":
-				value = value * 91.44
-				print " %c Centimeters" % value
-			else:
-				print "Not a valid choice."
+		if target == "Kilometer":
+			value = value * 0.0009144
+			print " %k Kilometers" % value
+		elif target == "Mile":
+			value = value * 0.000568182
+			print " %m Miles" % value
+		elif target == "Meter":
+			value = value * 0.9144
+			print " %m Meters" % value
+		elif target == "Foot":
+			value = value * 3
+			print " %f Feet" % value
+		elif target == "Inch":
+			value = value * 36
+			print " %i Inches" % value
+		elif target == "Centimeter":
+			value = value * 91.44
+			print " %c Centimeters" % value
+		else:
+			print "Not a valid choice."
 	elif base == "Foot":
 		value = int(raw_input("Enter base value: "))
 		target = raw_input("Enter target unit: ")
-			if target == "Kilometer":
-				value = value * 0.0003048
-				print " %k Kilometers" % value
-			elif target == "Mile":
-				value = value * 0.000189394
-				print " %m Miles" % value
-			elif target == "Meter":
-				value = value * 0.3048
-				print " %m Meters" % value
-			elif target == "Yard":
-				value = value * 0.333333
-				print " %y Yards" % value
-			elif target == "Inch":
-				value = value * 12
-				print " %i Inches" % value
-			elif target == "Centimeter":
-				value = value * 30.48
-				print " %c Centimeters" % value
-			else:
-				print "Not a valid choice."
+		if target == "Kilometer":
+			value = value * 0.0003048
+			print " %k Kilometers" % value
+		elif target == "Mile":
+			value = value * 0.000189394
+			print " %m Miles" % value
+		elif target == "Meter":
+			value = value * 0.3048
+			print " %m Meters" % value
+		elif target == "Yard":
+			value = value * 0.333333
+			print " %y Yards" % value
+		elif target == "Inch":
+			value = value * 12
+			print " %i Inches" % value
+		elif target == "Centimeter":
+			value = value * 30.48
+			print " %c Centimeters" % value
+		else:
+			print "Not a valid choice."
 	elif base == "Inch":
 		value = int(raw_input("Enter base value: "))
 		target = raw_input("Enter target unit: ")
-			if target == "Kilometer":
-				value = value * 0.833333
-				value = value * 0.0003048
-				print " %k Kilometers" % value
-			elif target == "Mile":
-				value = value * 0.833333
-				value = value * 0.000189394
-				print " %m Miles" % value
-			elif target == "Meter":
-				value = value * 0.0254
-				print " %m Meters" % value
-			elif target == "Yard":
-				value = value * 0.277778
-				print " %y Yards" % value
-			elif target == "Foot":
-				value = value * 0.0833333
-				print " %f Feet" % value
-			elif target == "Centimeter":
-				value = value * 2.54
-				print " %c Centimeters" % value
-			else:
-				print "Not a valid choice."
+		if target == "Kilometer":
+			value = value * 0.833333
+			value = value * 0.0003048
+			print " %k Kilometers" % value
+		elif target == "Mile":
+			value = value * 0.833333
+			value = value * 0.000189394
+			print " %m Miles" % value
+		elif target == "Meter":
+			value = value * 0.0254
+			print " %m Meters" % value
+		elif target == "Yard":
+			value = value * 0.277778
+			print " %y Yards" % value
+		elif target == "Foot":
+			value = value * 0.0833333
+			print " %f Feet" % value
+		elif target == "Centimeter":
+			value = value * 2.54
+			print " %c Centimeters" % value
+		else:
+			print "Not a valid choice."
 	elif base == "Centimeter":
 		value = int(raw_input("Enter base value: "))
 		target = raw_input("Enter target unit: ")
-			if target == "Kilometer":
-				value = value * 0.01
-				value = value * 0.001
-				print " %k Kilometers" % value
-			elif target == "Mile":
-				value = value * 0.0328084
-				value = value * 0.000189394
-				print " %m Miles" % value
-			elif target == "Meter":
-				value = value * 0.01
-				print " %m Meters" % value
-			elif target == "Yard":
-				value = value * 0.0109361
-				print " %y Yards" % value
-			elif target == "Foot":
-				value = value * 0.0328084
-				print " %f Feet" % value
-			elif target == "Inch":
-				value = value * 0.393701
-				print " %i Inches" % value
-			else:
-				print "Not a valid choice."
+		if target == "Kilometer":
+			value = value * 0.01
+			value = value * 0.001
+			print " %k Kilometers" % value
+		elif target == "Mile":
+			value = value * 0.0328084
+			value = value * 0.000189394
+			print " %m Miles" % value
+		elif target == "Meter":
+			value = value * 0.01
+			print " %m Meters" % value
+		elif target == "Yard":
+			value = value * 0.0109361
+			print " %y Yards" % value
+		elif target == "Foot":
+			value = value * 0.0328084
+			print " %f Feet" % value
+		elif target == "Inch":
+			value = value * 0.393701
+			print " %i Inches" % value
+		else:
+			print "Not a valid choice."
 	else:
 		print "Not a valid choice."
-#elif conv_type == "Length" or "2"
-#elif conv_type == "Mass" or "3"
-#elif conv_type == "Volume" or "4"
-#else:
-#	conv_type == "Digital Storage" or "5"
+#elif conv_type == "Mass" or "3":
+#elif conv_type == "Volume" or "4":
+#elif conv_type == "Digital Storage" or "5":
+else:
+	print "Not a valid choice."
