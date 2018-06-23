f3b69a6efc2cec3dd62bf0382a5b1222fdc70217
Austin Haedicke <austin.haedicke@gmail.com>
2018-06-23 11:19:18 -0400

    201806 monthly commit

--- android/build-kernel-savagezen.sh
+++ android/build-kernel-savagezen.sh
@@ -2,47 +2,58 @@
 # bulid kernel for Nexus 6P
 # https://forum.xda-developers.com/android/software-hacking/reference-how-to-compile-android-kernel-t3627297
 
-BRANCH_UPS=oreo-m3-release
-ORIGINAL_OWNER=copperheados
-ORIGINAL_REPO=kernel_huawei_angler
+COPPERHEAD_BRANCH=oreo-m3-release
+COPPERHEAD_REPO=https://github.com/AndroidHardening/kernel_huawei_angler
+BRANCH_KERNEL=oreo-m3-personal
 
 # merge copperheadOS upstream
-# $ git checkout $BRANCH_UPS
-# $ git pull https://github.com/$ORIGINAL_OWNER/$ORIGINAL_REPO.git $BRANCH_UPS
-# add, commit, and push (to origin/$BRANCH_UPS)
+# $ git checkout $COPPERHEAD_BRANCH
+# $ git pull $COPPERHEAD_REPO $COPPERHEAD_BRANCH
+# add, commit, and push
 # $ git checkout $BRANCH_KERNEL
-# $ git merge $BRANCH_UPS
+# $ git merge $COPPERHEAD_BRANCH
 # $ git push
 
-# merge upstream into personal release
-# $ git checkout oreo-m3-personal
-# $ git merge oreo-m3-release
+LINUX_STABLE_URL=https://github.com/android-linux-stable/angler
+LINUX_STABLE_BRANCH=android-msm-angler-3.10
+
+# merge linux-stable
+# https://github.com/android-linux-stable/notes
+# https://github.com/android-linux-stable/notes/blob/master/trees/angler.md
+# $ git checkout linux-stable
+# (initial) $ git remote add linux-stable $LINUX_STABLE_URL
+# $ git fetch linux-stable $LINUX_STABLE_BRANCH
+# $ git merge FETCH_HEAD
+
+# new branches
+# new branch: $ git checkout -b new_branch
+# copy and create: $ git checkout -b new_branch old_branch
+# $ git push origin new_branch
+
+# clean demon files from AK
+# wget https://github.com/nathanchance/AnyKernel2/commit/addb6ea860aab14f0ef684f6956d17418f95f29a.diffpatch -p1 < addb6ea860aab14f0ef684f6956d17418f95f29a.diffrm addb6ea860aab14f0ef684f6956d17418f95f29a.diff
 
 # merge AnyKernel2 updates
 # $ cd ~/git/anykernel
 # $ git checkout master
 # $ git pull https://github.com/osm0sis/AnyKernel2.git
-# add, commit, and push
+# $ git push
 # $ git checkout angler-personal
 # $ git merge master 
 # $ git push
 
-BRANCH_KERNEL=oreo-m3-personal
-BRANCH_AK=angler-personal
-SRC_PREBUILT=https://android.googlesource.com/platform/prebuilt
+NAME=angler-kernel-$(date +%Y%m%d)
 SRC_KERNEL=https://github.com/savagezen/kernel_huawei_angler
-#SRC_KERNEL=https://android.googlesource.com/kernel/msm
 SRC_AK=https://github.com/savagezen/anykernel
-TOOLCHAIN=$(pwd)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+BRANCH_AK=angler-personal
+PREBUILT=https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
+TOOLCHAIN=$(pwd)/prebuilt/bin/aarch64-linux-android-
 
 ## pull source ##
-git clone $SRC_PREBUILT
+git clone $PREBUILT
+# or uptedate tools: $ cd ~/git/prebulits; git pull
 git clone -b $BRANCH_KERNEL $SRC_KERNEL
 git clone -b $BRANCH_AK $SRC_AK
-
-# to copy and create new branch
-# $ git checkout -b new_branch old_branch
-# $ git push origin new_branch
 
 # setup environment
 export CROSS_COMPILE=$TOOLCHAIN
@@ -59,8 +70,8 @@ make savagezen_defconfig
 # build kernel
 time make -j2
 cp -v arch/arm64/boot/Image.gz-dtb ../anykernel/zImage-dtb
-cp -v arch/arm64/boot/Image.gz-dtb ../aosp_device_huawei_angler-kernel/
+#cp -v arch/arm64/boot/Image.gz-dtb ../aosp_device_huawei_angler-kernel/
 cd ../anykernel
 
 # package
-zip -r9 kernel_$(date +%Y%m%d).zip * -x README.md kernel_$(date +%Y%m%d).zip
+zip -r9 $NAME.zip * -x README.md $NAME.zip
