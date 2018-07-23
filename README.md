2e86a7e17f533c956620185bc23fa5a5aac397fe
Austin Haedicke <austin.haedicke@gmail.com>
2018-07-23 17:26:49 -0400

    monthly push 201807

--- README.md
+++ README.md
@@ -1,47 +1,4 @@
 Command Line Utilities
 ====================
 
-### Android Governor Profiles (stable)
-* [butterfly](https://github.com/gtbjj/scripts/blob/master/android/interactive-butterfly.sh)
-* [cyan lion](https://github.com/gtbjj/scripts/blob/master/android/interactive-cyanlion_r6.sh)
-* [dark spice](https://github.com/gtbjj/scripts/blob/master/interactive-darkspice_7.5.sh)
-* [eclipse](https://github.com/gtbjj/scripts/blob/master/android/interactive-eclipse_r3.sh)
-* [ghost pepper](https://github.com/gtbjj/scripts/blob/master/android/interactive-ghostpepper_v1.1.sh)
-* [glass fish](https://github.com/gtbjj/scripts/blob/master/android/interactive-glassfish_v1.2.sh)
-* [heimdall](https://github.com/gtbjj/scripts/blob/master/android/interactive-heimdall_v5.sh)
-* [loki](https://github.com/gtbjj/scripts/blob/master/android/elementalx-loki_v1.sh)
-* [mad dog](https://github.com/gtbjj/scripts/blob/master/android/interactive-maddog.sh)
-* [silver fish](https://github.com/gtbjj/scripts/blob/master/android/silverfish_v2.1.sh)
-
-### Android Governor Profiles (alpha / beta)
-* [austin](https://github.com/gtbjj/scripts/blob/master/android/interactive-austin.sh)
-* [cyan lion remix 1](https://github.com/gtbjj/scripts/blob/master/android/interactive-clrmx1.sh)
-* [cyan lion remix 2](https://github.com/gtbjj/scripts/blob/master/android/interactive-clrmx2.sh)
-* [cyan lion remix 3](https://github.com/gtbjj/scripts/blob/master/android/interactive-clrmx3.sh)
-* [hawk tail](https://github.com/gtbjj/scripts/blob/master/android/interactive-hawktail_alpha.sh)
-* [heimdall remix 1](https://github.com/gtbjj/scripts/blob/master/android/interactive-hmdrmx1.sh)
-* [heimdall remix 2](https://github.com/gtbjj/scripts/blob/master/android/ineractive-hmdrmx2.sh)
-* [heimdall remix 3](https://github.com/gtbjj/scripts/blob/master/android/interactive-hmdrmx3.sh)
-
-#### Shell (Linux)
-* [AUR Package Install](https://github.com/gtbjj/scripts/blob/master/aur-get.sh)
-* [AUR Package Update](https://github.com/gtbjj/scripts/blob/master/aur-update.sh)
-* [Rip / Burn / Play DVDs](https://github.com/gtbjj/scripts/blob/master/dvd-tools.sh)
-* [Sync Media from Linux to Removable (Anroid) SD Card](https://github.com/gtbjj/scripts/blob/master/sd-sync.sh)
-* [Sync Music from Linux to Android over SSH](https://github.com/gtbjj/scripts/blob/master/ssh-music.sh)
-* [System Information](https://github.com/gtbjj/scripts/blob/master/sysinfo.sh)
-* [Unlock (gpg) and Unzip (gz) File](https://github.com/grandtheftjiujisu/scripts/blob/master/ungpg.sh)
-* [WiFi Set Up Using Netctl](https://github.com/gtbjj/scripts/blob/master/wifi-netctl.sh)
-* [WPA / Wifi setup using "iw" and "dhcpcd"](https://github.com/gtbjj/scripts/blob/master/wifi-wpa.sh)
-
-#### Python (3.x)
-* My Dungeons and Dragons scripts are now compiled as an AUR package.  See [Source Code](https://github.com/gtbjj/dnd-tools) and [PKGBUILD](https://github.com/gtbjj/pkgbuild).
-
-#### Python (2.7)
-* [Unit Conversions](https://github.com/gtbjj/scripts/blob/master/unit_conversion.py)
-* [Climbing Grade Conversions](https://github.com/gtbjj/scripts/blob/master/climbing_grade_conversion.py)
-* Dungeons & Dragons Charcter Generator (deprecated by 5e versions above)
-* RPG Dice Roller (included feature in D&D Character Generator for Python 3.x)
-
-#### Perl6
-* Demos from the [Perl6 Maven Tutorial Series](http://perl6maven.com/tutorial/toc)
+Remote repository for my personal handmade tools.
--- android/build-kernel-savagezen.sh
+++ android/build-kernel-savagezen.sh
@@ -2,76 +2,57 @@
 # bulid kernel for Nexus 6P
 # https://forum.xda-developers.com/android/software-hacking/reference-how-to-compile-android-kernel-t3627297
 
-COPPERHEAD_BRANCH=oreo-m3-release
-COPPERHEAD_REPO=https://github.com/AndroidHardening/kernel_huawei_angler
-BRANCH_KERNEL=oreo-m3-personal
+NAME=angler-kernel-$(date +%Y%m%d)
+KERNEL_SOURCE=https://github.com/savagezen/kernel_huawei_angler
 
-# merge copperheadOS upstream
-# $ git checkout $COPPERHEAD_BRANCH
-# $ git pull $COPPERHEAD_REPO $COPPERHEAD_BRANCH
-# add, commit, and push
-# $ git checkout $BRANCH_KERNEL
-# $ git merge $COPPERHEAD_BRANCH
-# $ git push
+AK_SOURCE=https://github.com/savagezen/anykernel
+AK_BRANCH=angler-personal
 
-LINUX_STABLE_URL=https://github.com/android-linux-stable/angler
+PREBUILT=https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
+TOOLCHAIN=$(pwd)/prebuilts/bin/aarch64-linux-android-
+
+LINUX_STABLE_SOURCE=https://github.com/android-linux-stable/angler
 LINUX_STABLE_BRANCH=android-msm-angler-3.10
 
-# merge linux-stable
-# https://github.com/android-linux-stable/notes
-# https://github.com/android-linux-stable/notes/blob/master/trees/angler.md
-# $ git checkout linux-stable
-# (initial) $ git remote add linux-stable $LINUX_STABLE_URL
-# $ git fetch linux-stable $LINUX_STABLE_BRANCH
-# $ git merge FETCH_HEAD
+FK_BRANCH=oreo-mr1
+FK_SOURCE=https://github.com/franciscofranco/angler
 
-# new branches
-# new branch: $ git checkout -b new_branch
-# copy and create: $ git checkout -b new_branch old_branch
-# $ git push origin new_branch
-
-# clean demon files from AK
-# wget https://github.com/nathanchance/AnyKernel2/commit/addb6ea860aab14f0ef684f6956d17418f95f29a.diffpatch -p1 < addb6ea860aab14f0ef684f6956d17418f95f29a.diffrm addb6ea860aab14f0ef684f6956d17418f95f29a.diff
-
-# merge AnyKernel2 updates
-# $ cd ~/git/anykernel
-# $ git checkout master
-# $ git pull https://github.com/osm0sis/AnyKernel2.git
-# $ git push
-# $ git checkout angler-personal
-# $ git merge master 
-# $ git push
-
-NAME=angler-kernel-$(date +%Y%m%d)
-SRC_KERNEL=https://github.com/savagezen/kernel_huawei_angler
-SRC_AK=https://github.com/savagezen/anykernel
-BRANCH_AK=angler-personal
-PREBUILT=https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
-TOOLCHAIN=$(pwd)/prebuilt/bin/aarch64-linux-android-
-
-## pull source ##
+# pull source
 git clone $PREBUILT
-# or uptedate tools: $ cd ~/git/prebulits; git pull
-git clone -b $BRANCH_KERNEL $SRC_KERNEL
-git clone -b $BRANCH_AK $SRC_AK
+git clone $KERNEL_SOURCE -b $FK_BRANCH
+git clone $AK_SOURCE -b $AK_BRANCH
 
-# setup environment
-export CROSS_COMPILE=$TOOLCHAIN
+# set up environment
 export ARCH=arm64
 export SUBARCH=arm64
-
-## configure kernel ##
+export CROSS_COMPILE=$TOOLCHAIN
 cd kernel_huawei_angler
+
+# merge linux-stable
+## https://github.com/android-linux-stable/notes
+## https://github.com/android-linux-stable/notes/blob/master/trees/angler.md
+git fetch $LINUX_STABLE_MERGE $LINUX_STABLE_BRANCH
+git merge FETCH_HEAD
+## add, commit, push
+
+# merge franco kernel upstream
+git fetch $FK_SOURCE $FK_BRANCH
+git merge FETCH_HEAD
+## add, commit, push
+
+# configure kernel #
 make clean
 make mrproper
 make savagezen_defconfig
-# make xconfig -> update VERSION string
+## make xconfig -> update VERSION string
 
 # build kernel
 time make -j2
-cp -v arch/arm64/boot/Image.gz-dtb ../anykernel/zImage-dtb
-#cp -v arch/arm64/boot/Image.gz-dtb ../aosp_device_huawei_angler-kernel/
-cd ../anykernel
+## cp -v arch/arm64/boot/Image.gz-dtb ../aosp_device_huawei_angler-kernel/
 
 # package
+cd ../anykernel
+## clean demon files from AK
+## wget https://github.com/nathanchance/AnyKernel2/commit/addb6ea860aab14f0ef684f6956d17418f95f29a.diffpatch -p1 < addb6ea860aab14f0ef684f6956d17418f95f29a.diffrm addb6ea860aab14f0ef684f6956d17418f95f29a.diff
+cp -v kernel_huawei_angler/arch/arm64/boot/Image.gz-dtb zImage-dtb
 zip -r9 $NAME.zip * -x README.md $NAME.zip
